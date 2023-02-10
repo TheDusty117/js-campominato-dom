044cf94c0c8d778215d93e43e1356e1185c8d0a9
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-10 18:27:53 +0100

    FEAT need to comunicate value to bomber

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ document.querySelector('.play-button').addEventListener('click',function(){ // A
   // CICLO CHE GENERA LE 100 CASELLE ------------------------------------------------------
   for (let i = 0; i < numeroCelle; i++) {
     let num = i + 1 
-    console.log(i + 1)
+    // console.log(i + 1)
     const cellaElement = document.createElement('div')
     cellaElement.className = 'cella'
     
@@ -41,40 +41,36 @@ document.querySelector('.play-button').addEventListener('click',function(){ // A
     
     cellaElement.addEventListener('click', function(event) { 
       cellaElement.style.backgroundColor = 'cornflowerblue'
-      console.log(`sono un click ${num}`, typeof cellaElement)
-      // console.log(numeroCelle)
-      console.log(num)
-      console.log(event.target)// CONSOLE LOG DI EVENTO E IL SUO TARGET
+      console.log(bombeArr)
+      console.log(this.innerHTML)
+    
+
+      let valueNum = parseInt(this.innerHTML)
+      console.log(valueNum)
+
+
+      //CONFRONTARE valueNum CON array di BOMBE
+
+  
     })
   }
   // FINE CICLO CHE GENERA LE 100 CASELLE --------------------------------------------------------
- 
-  
-})
-
-
-
-grigliaElement.addEventListener('click',function(){ //CON QUESTA FUNZIONE, CREO UNA LISTA DI 16 NUMERI al click, questa lista di 16 numeri, se corrispondente al numero casella, SARA' una BOMBA
-
-  //CREARE UN ARRAY DI 16 NUMERI CHE SONO SEMPRE CASUALI (DA 1 A 100) quindi (array= [1,30,22,8,6,13,50,99,35,23.....])
-  
-  //CREO ARRAY VUOTO --------
-  
   const bombeArr = [] //ARRAY BOMBE RANDOM X 16
-  // console.log(bombeArr)
   
-
+  
   //FACCIO UN CICLO CHE CREA 16 NUMERI CASUALI(da 1 a 100), ed ognuno di loro verra' PUSHATO DENTRO bombeArr che si trova qui su----------------
   for (let i = 0; i < 16; i++) {
     const numRandom = Math.floor(Math.random(1)*100+1)
     // console.log(numRandom)
     bombeArr.push(numRandom)  //ogni numero random(da 0 a 100) viene pushato nell'array bombe sopra per 16 volte
   }
-  
-
   console.log(bombeArr)
   
+  
 })
+
+
+
 
 
 
